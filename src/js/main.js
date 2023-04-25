b4590e73524aa8a71f240d21e6eb820e55979695
RikWijtman <1045768@hr.nl>
2023-04-25 15:10:31 +0200

    jippie

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 import '../css/style.css'
-import fishImage from '../images/fish.png'
+import fish1 from '../images/Freddi.png'
+//import fish2 from '../images/Luther.png'
 
 const div = document.createElement("div")
 div.classList.add("fish")
 document.body.appendChild(div)
 
 const img = document.createElement("img")
-img.src = fishImage
+img.src = fish1
 div.appendChild(img)
